fc5c7e41d20a9de46581d0e2546973406fc6e41c
jcardser <jcardser@gmail.com>
2025-03-10 22:19:06 -0500

    A new branch "HomeController" is added, a new class is added

---
+++ Ecommerce.API/Ecommerce.API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string MSNTexto()
+        {
+            return "Hello World desde la nueva funcionalidad";
+        }
+    }
+}
